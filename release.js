1377874f2f81f8f580ebe2da0199313e622ef484
Joey J <joey.jaulimsing@live.com>
2021-09-16 12:16:47 +0400

    Release v1.1.0

---
+++ release.js
@@ -0,0 +1,23 @@
+(async function () {
+    const { execSync } = require('child_process')
+    const prompt = require('prompt')
+    const fs = require('fs')
+
+    // read package.json
+    // get version
+    const currentVersion = execSync('git describe --tags --abbrev=0').toString()
+
+    // prompt user for version
+    prompt.start()
+    const { version } = await prompt.get({
+        properties: {
+            version: {
+                description: 'Enter the version to publish:',
+                default: currentVersion,
+                before: function(value) { return 'v' + value; }
+            }
+        }
+    })
+
+    execSync(`git add . & git commit -m "Release ${version}"`)
+}())
