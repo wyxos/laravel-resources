f698b49760f5225c7a1de2cc36d016359a78daf2
Joey J <joey.jaulimsing@live.com>
2021-11-02 14:20:09 +0400

    Release v1.3.0

--- src/ResourceTestHelpers.php
+++ src/ResourceTestHelpers.php
@@ -6,15 +6,23 @@ use Illuminate\Testing\TestResponse;
 
 trait ResourceTestHelpers
 {
-    protected function getResource($resource, $handler, $params = []): TestResponse
+    protected function getResource($path, $params = []): TestResponse
     {
+        list($resource, $handler) = explode('.', $path, 2);
+
+        $handler = preg_replace('/\./', '/', $handler);
+
         $merge = array_merge([$resource, $handler], $params);
 
         return $this->get(route('resource', $merge));
     }
 
-    protected function postResource($resource, $handler, $data = []): TestResponse
+    protected function postResource($path, $data = []): TestResponse
     {
+        list($resource, $handler) = explode('.', $path, 2);
+
+        $handler = preg_replace('/\./', '/', $handler);
+
         return $this->post(route('resource', [$resource, $handler]), $data);
     }
 }
