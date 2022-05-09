ad16fefb9cc82f79b8517051cbfa33939c7c9278
Joey J <joey.jaulimsing@live.com>
2022-05-09 15:33:06 +0400

    Release v1.4.0

--- release.js
+++ release.js
@@ -1,5 +1,6 @@
 (async function () {
     const { execSync } = require('child_process')
+    const fs = require('fs')
     const prompt = require('prompt')
 
     // read package.json
@@ -12,21 +13,28 @@
         properties: {
             version: {
                 description: 'Enter the version to publish:',
-                default: currentVersion,
-                before: function(value) { return 'v' + value; }
+                default: currentVersion
             }
         }
     })
 
+    const json = JSON.parse(fs.readFileSync('package.json').toString())
+
+    json.version = version
+
+    fs.writeFileSync('./package.json', JSON.stringify(json, null, 2))
+
+    const tagVersion = `v${version}`
+
     execSync('git add .')
     console.log('Files staged.')
 
-    execSync(`git commit -m "Release ${version}"`)
+    execSync(`git commit -m "Release ${tagVersion}"`)
     console.log('Files committed.')
 
-    execSync(`git tag ${version}`)
+    execSync(`git tag ${tagVersion}`)
     console.log('Tag version added.')
 
-    execSync(`git push origin ${version}`)
+    execSync(`git push origin ${tagVersion}`)
     console.log('Push complete.')
 }())
